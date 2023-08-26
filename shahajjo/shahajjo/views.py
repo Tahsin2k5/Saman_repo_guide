d7613000fdb91e01c2b733ddfd54ffec9757b133
Tahsin2k5 <tahsin2k5@gmail.com>
2023-08-26 21:30:09 +0600

    Self commit one

--- shahajjo/shahajjo/views.py
+++ shahajjo/shahajjo/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import HttpResponse, render
 def index(request):
     return render(request,'admin/cat.html')
 def insert(request):
-    cat_name = request.POST.get('cat_name')
-    return HttpResponse(cat_name)
+    name = request.POST.get('name')
+    gender = request.POST.get('gender')
+    phn_number = request.POST.get('phn_number')
+    occupation = request.POST.get('occupation')
+    religion = request.POST.get('religion')
+    gender = request.POST.get('gender')
+    bank_no = request.POST.get('bank_no')
+
+    
+    return HttpResponse(name)
